5c98c4df8af6ba726b656e2da76384c5fd1aea6c
rdbo <rdbodev@gmail.com>
2021-02-27 21:53:34 -0300

    added optional override functions and improved types

--- cgui/cgui.hpp
+++ cgui/cgui.hpp
@@ -11,12 +11,20 @@
 /* Includes */
 #include <cmath>
 #include <string>
+#include <array>
+#include <vector>
 
 /* Defines */
 #define CGUI_FALSE false
 #define CGUI_TRUE  true
 #define CGUI_PI 3.141592654f
 #define CGUI_CIRCLE_VERTICES 10
+
+/* Helpers */
+#define CGUI_SIN(ang) sin(ang)
+#define CGUI_SIN2(ang) CGUI_SIN(ang / 180.0f * CGUI_PI)
+#define CGUI_COS(ang) cos(ang)
+#define CGUI_COS2(ang) CGUI_COS(ang / 180.0f * CGUI_PI)
 
 /* Types */
 typedef void                                     cgui_void;
@@ -32,133 +40,22 @@ typedef cgui_uint                                cgui_size;
 typedef std::basic_string<cgui_char>             cgui_string;
 
 /* Classes */
-template <cgui_size length>
-class cgui_array
+template <typename cgui_type, cgui_size _length>
+class cgui_array : public std::array<cgui_type, _length>
 {
 	/*
 	 * Generic array with
 	 * static length
 	 */
-private:
-	cgui_float data[length];
-public:
-	cgui_array()
-	{
-
-	}
-
-	~cgui_array()
-	{
-
-	}
-public:
-	inline cgui_float operator[](cgui_size pos)
-	{
-		cgui_float item = 0.0f;
-		if (pos < (sizeof(this->data) / sizeof(this->data[0])))
-			item = this->data[pos];
-		return item;
-	}
-
-	inline cgui_bool operator==(cgui_array<length>& arr)
-	{
-		cgui_bool ret = CGUI_TRUE;
-
-		for (cgui_size i = 0; i < length; ++i)
-		{
-			ret = ((*this)[i] == arr[i] ? ret : CGUI_FALSE);
-			if (ret == CGUI_FALSE) break;
-		}
-
-		return ret;
-	}
 };
 
 template <typename cgui_type>
-class cgui_vector
+class cgui_vector : public std::vector<cgui_type>
 {
 	/*
 	 * Generic vector with
 	 * dynamic length
 	 */
-private:
-	cgui_size len = 0;
-	cgui_type *buf = (cgui_type *)nullptr;
-public:
-	inline cgui_vector()
-	{
-
-	}
-
-	inline ~cgui_vector()
-	{
-		if (this->buf)
-			delete[] this->buf;
-	}
-public:
-	cgui_type operator[](cgui_size pos)
-	{
-		cgui_type ret = (cgui_type)0;
-		if (pos < this->length())
-			ret = this->data()[pos];
-		return ret;
-	}
-
-	cgui_bool operator==(cgui_vector<cgui_type>& vec)
-	{
-		cgui_bool ret = CGUI_FALSE;
-		if (vec.length() != this->length()) return ret;
-
-		ret = CGUI_TRUE;
-
-		for (cgui_size i = 0; i < length; ++i)
-		{
-			ret = ((*this)[i] == vec[i] ? ret : CGUI_FALSE);
-			if (ret == CGUI_FALSE) break;
-		}
-
-		return ret;
-	}
-
-public:
-	inline cgui_size length()
-	{
-		return this->len;
-	}
-
-	inline cgui_type *data()
-	{
-		return this->buf;
-	}
-
-	inline cgui_void push(cgui_type value)
-	{
-		cgui_type *holder = this->data();
-		this->buf = new cgui_type[this->length() + 1];
-		if (holder)
-		{
-			for (cgui_size i = 0; i < this->length(); ++i)
-				this->buf[i] = holder[i];
-			delete[] holder;
-		}
-
-		++this->len;
-	}
-
-	inline cgui_type pop()
-	{
-		cgui_type ret = (cgui_type)0;
-		if (this->length() == 0) return ret;
-
-		cgui_type *holder = this->data();
-		this->buf = new cgui_type[this->length() - 1];
-		if (holder)
-		{
-			for (cgui_size i = 0; i < this->length() - 1; ++i)
-				this->buf[i] = holder[i];
-			delete[] holder;
-		}
-	}
 };
 
 class cgui_color
@@ -228,45 +125,11 @@ public:
 	}
 };
 
-class cgui_font
+class cgui_font : public cgui_vector<cgui_byte>
 {
-private:
-	cgui_byte *data = (cgui_byte *)nullptr;
-public:
-	inline cgui_font()
-	{
-
-	}
-
-	inline cgui_font(cgui_byte *buffer, cgui_size size)
-	{
-		this->Create(buffer, size);
-	}
-
-	inline ~cgui_font()
-	{
-		if(this->data)
-			this->Free();
-	}
-public:
-	inline cgui_void Create(cgui_byte *buffer, cgui_size size)
-	{
-		if (this->data)
-			this->Free();
-		this->data = new cgui_byte[size];
-		for (cgui_size i = 0; i < size; ++i)
-			this->data[i] = buffer[i];
-	}
-
-	inline cgui_byte *GetData()
-	{
-		return this->data;
-	}
-
-	inline cgui_void Free()
-	{
-		delete[] this->data;
-	}
+	/*
+	 * Generic font
+	 */
 };
 
 class cgui_handle
@@ -284,20 +147,119 @@ class cgui_handle
 	 */
 public:
 	/* Mandatory Override */
-	virtual inline cgui_void DrawLine(cgui_array<2> point0, cgui_array<2> point1, cgui_color color, cgui_float thickness) {  }
-	virtual inline cgui_void DrawFilledRectangle(cgui_array<2> min, cgui_array<2> max, cgui_color color) {  }
-	virtual inline cgui_void DrawFilledTriangle(cgui_array<2> vertex0, cgui_array<2> vertex1, cgui_array<2> vertex2, cgui_color color) {  }
-	virtual inline cgui_void DrawText(cgui_string text, cgui_font font, cgui_array<2> position, cgui_color color, cgui_size size) {  }
+	virtual inline cgui_void DrawLine(cgui_array<cgui_float, 2> point0, cgui_array<cgui_float, 2> point1, cgui_color color, cgui_float thickness) {  }
+	virtual inline cgui_void DrawFilledTriangle(cgui_array<cgui_float, 2> vertex0, cgui_array<cgui_float, 2> vertex1, cgui_array<cgui_float, 2> vertex2, cgui_color color) {  }
+	virtual inline cgui_void DrawText(cgui_string text, cgui_font font, cgui_array<cgui_float, 2> position, cgui_color color, cgui_size size) {  }
 
 	/* Optional Override */
-	virtual inline cgui_void DrawRectangle(cgui_array<2> min, cgui_array<2> max, cgui_color color, cgui_float thickness = 1.0f);
-	virtual inline cgui_void DrawQuad(cgui_array<2> point0, cgui_array<2> point1, cgui_array<2> point2, cgui_array<2> point3, cgui_color color, cgui_float thickness = 1.0f);
-	virtual inline cgui_void DrawFilledQuad(cgui_array<2> point0, cgui_array<2> point1, cgui_array<2> point2, cgui_array<2> point3, cgui_color color);
-	virtual inline cgui_void DrawTriangle(cgui_array<2> vertex0, cgui_array<2> vertex1, cgui_array<2> vertex2, cgui_color color, cgui_float thickness = 1.0f);
-	virtual inline cgui_void DrawCircle(cgui_array<2> center, cgui_float radius, cgui_color color, cgui_float thickness = 1.0f);
-	virtual inline cgui_void DrawFilledCircle(cgui_array<2> center, cgui_float radius, cgui_color color);
-	virtual inline cgui_void DrawPolygon(cgui_vector<cgui_array<2>> vertices, cgui_color color, cgui_float thickness = 1.0f);
-	virtual inline cgui_void DrawFilledPolygon(cgui_vector<cgui_array<2>> vertices, cgui_color color);
+	virtual inline cgui_void DrawRectangle(cgui_array<cgui_float, 2> min, cgui_array<cgui_float, 2> max, cgui_color color, cgui_float thickness = 1.0f)
+	{
+		cgui_array<cgui_float, 2> p0, p1, p2, p3;
+		p0 = min;
+		p1 = p0;
+		p1[1] = max[1];
+		p2 = p1;
+		p2[0] = max[0];
+		p3 = max;
+
+		this->DrawQuad(p0, p1, p2, p3, color, thickness);
+	}
+
+	virtual inline cgui_void DrawFilledRectangle(cgui_array<cgui_float, 2> min, cgui_array<cgui_float, 2> max, cgui_color color)
+	{
+		cgui_array<cgui_float, 2> p0, p1, p2, p3;
+		p0 = min;
+		p1 = p0;
+		p1[1] = max[1];
+		p2 = p1;
+		p2[0] = max[0];
+		p3 = max;
+
+		this->DrawFilledQuad(p0, p1, p2, p3, color);
+	}
+
+	virtual inline cgui_void DrawQuad(cgui_array<cgui_float, 2> point0, cgui_array<cgui_float, 2> point1, cgui_array<cgui_float, 2> point2, cgui_array<cgui_float, 2> point3, cgui_color color, cgui_float thickness = 1.0f)
+	{
+		this->DrawLine(point0, point1, color, thickness);
+		this->DrawLine(point1, point2, color, thickness);
+		this->DrawLine(point2, point3, color, thickness);
+		this->DrawLine(point3, point0, color, thickness);
+	}
+
+	virtual inline cgui_void DrawFilledQuad(cgui_array<cgui_float, 2> point0, cgui_array<cgui_float, 2> point1, cgui_array<cgui_float, 2> point2, cgui_array<cgui_float, 2> point3, cgui_color color)
+	{
+		this->DrawFilledTriangle(point0, point1, point2, color);
+		this->DrawFilledTriangle(point1, point2, point3, color);
+		this->DrawFilledTriangle(point0, point1, point3, color);
+	}
+
+	virtual inline cgui_void DrawTriangle(cgui_array<cgui_float, 2> vertex0, cgui_array<cgui_float, 2> vertex1, cgui_array<cgui_float, 2> vertex2, cgui_color color, cgui_float thickness = 1.0f)
+	{
+		this->DrawLine(vertex0, vertex1, color, thickness);
+		this->DrawLine(vertex1, vertex2, color, thickness);
+		this->DrawLine(vertex2, vertex0, color, thickness);
+	}
+
+	virtual inline cgui_void DrawCircle(cgui_array<cgui_float, 2> center, cgui_float radius, cgui_color color, cgui_float thickness = 1.0f)
+	{
+		cgui_int vertices = (cgui_int)(CGUI_CIRCLE_VERTICES * radius);
+		if (!vertices) return;
+		cgui_float angle = (360.0f / vertices);
+		cgui_array<cgui_float, 2> old_point;
+		for (cgui_float cur_angle = 0.0f; cur_angle < 360.0f; cur_angle += angle)
+		{
+			cgui_float dist_sin = CGUI_SIN2(cur_angle) * radius;
+			cgui_float dist_cos = CGUI_COS2(cur_angle) * radius;
+			cgui_array<cgui_float, 2> cur_point;
+			cur_point[0] = old_point[0] + dist_sin;
+			cur_point[1] = old_point[1] + dist_cos;
+			if (cur_angle != 0.0f)
+				this->DrawLine(old_point, cur_point, color, thickness);
+			old_point = cur_point;
+		}
+	}
+
+	virtual inline cgui_void DrawFilledCircle(cgui_array<cgui_float, 2> center, cgui_float radius, cgui_color color)
+	{
+		cgui_int vertices = (cgui_int)(CGUI_CIRCLE_VERTICES * radius);
+		if (!vertices) return;
+		cgui_float angle = (360.0f / vertices);
+		cgui_array<cgui_float, 2> old_point = {  };
+		for (cgui_float cur_angle = 0.0f; cur_angle < 360.0f; cur_angle += angle)
+		{
+			cgui_float dist_sin = CGUI_SIN2(cur_angle) * radius;
+			cgui_float dist_cos = CGUI_COS2(cur_angle) * radius;
+			cgui_array<cgui_float, 2> cur_point = {  };
+			cur_point[0] = old_point[0] + dist_sin;
+			cur_point[1] = old_point[1] + dist_cos;
+			if (cur_angle != 0.0f)
+				this->DrawFilledTriangle(center, old_point, cur_point, color);
+			old_point = cur_point;
+		}
+	}
+
+	virtual inline cgui_void DrawPolygon(cgui_vector<cgui_array<cgui_float, 2>> vertices, cgui_color color, cgui_float thickness = 1.0f)
+	{
+		for (cgui_size i = 0; i < vertices.size() && i > 0; ++i)
+		{
+			cgui_array<cgui_float, 2> &vertex0 = vertices[i - 1];
+			cgui_array<cgui_float, 2> &vertex1 = vertices[i];
+
+			this->DrawLine(vertex0, vertex1, color, thickness);
+		}
+	}
+
+	virtual inline cgui_void DrawFilledPolygon(cgui_vector<cgui_array<cgui_float, 2>> vertices, cgui_color color)
+	{
+		for (cgui_size i = 0; i < vertices.size() && i >= 2; ++i)
+		{
+			cgui_array<cgui_float, 2> &vertex0 = vertices[i - 2];
+			cgui_array<cgui_float, 2> &vertex1 = vertices[i - 1];
+			cgui_array<cgui_float, 2> &vertex2 = vertices[i];
+
+			this->DrawFilledTriangle(vertex0, vertex1, vertex2, color);
+		}
+	}
 };
 
 #endif
