5143b7d1576329af24c8a634751f018837bf6607
rdbo <rdbodev@gmail.com>
2021-02-27 23:51:44 -0300

    added draw filled rect (dx9)

--- cgui/cgui_impl_dx9.hpp
+++ cgui/cgui_impl_dx9.hpp
@@ -21,18 +21,24 @@ public:
 public:
     inline cgui_void DrawLine(cgui_array<cgui_float, 2> point0, cgui_array<cgui_float, 2> point1, cgui_color color, cgui_float thickness) final
     {
-        ID3DXLine *pD3DXLine = (ID3DXLine *)nullptr;
-        D3DXCreateLine(this->pD3DDevice, &pD3DXLine);
-        if (pD3DXLine)
+        ID3DXLine *pDXLine = (ID3DXLine *)nullptr;
+        D3DXCreateLine(this->pD3DDevice, &pDXLine);
+        if (pDXLine)
         {
             D3DXVECTOR2 points[2] = {};
             points[0] = { (FLOAT)point0[0], (FLOAT)point0[1] };
             points[1] = { (FLOAT)point1[0], (FLOAT)point1[1] };
 
-            pD3DXLine->SetWidth(thickness);
-            pD3DXLine->Draw(points, 2, D3DCOLOR_RGBA(color.Red(), color.Green(), color.Blue(), color.Alpha()));
-            pD3DXLine->Release();
+            pDXLine->SetWidth(thickness);
+            pDXLine->Draw(points, 2, D3DCOLOR_RGBA(color.Red(), color.Green(), color.Blue(), color.Alpha()));
+            pDXLine->Release();
         }
+    }
+
+    inline cgui_void DrawFilledRectangle(cgui_array<cgui_float, 2> min, cgui_array<cgui_float, 2> max, cgui_color color)
+    {
+        D3DRECT DXRect = { (LONG)min[0], (LONG)min[1], (LONG)max[0], (LONG)max[1] };
+        this->pD3DDevice->Clear(1, &DXRect, D3DCLEAR_TARGET, D3DCOLOR_RGBA(color.Red(), color.Green(), color.Blue(), color.Alpha()), 0.0f, 0);
     }
 };
 
